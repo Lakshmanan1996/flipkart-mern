1b8a2aecc81f425abee07dd78182e208ab8f2a8e
Lakshmanan1996 <117020820+Lakshmanan1996@users.noreply.github.com>
2026-02-10 12:35:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-const server = app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
+const server = app.listen(PORT, '0.0.0.0',() => {
+    console.log(`Server running on ${PORT}`)
 });
 
 // Unhandled Promise Rejection
